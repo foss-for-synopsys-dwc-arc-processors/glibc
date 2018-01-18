e247cd23500e94ad3fe5604a7fc192f2b4c1d1c0
Vineet Gupta <vgupta@synopsys.com>
2018-01-18 10:08:45 -0800

    ARC: update copyright to 2018
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- sysdeps/unix/sysv/linux/arc/kernel-features.h
+++ sysdeps/unix/sysv/linux/arc/kernel-features.h
@@ -1,7 +1,7 @@
 /* Set flags signalling availability of kernel features based on given
    kernel version number.
 
-   Copyright (C) 2009-2017 Free Software Foundation, Inc.
+   Copyright (C) 2009-2018 Free Software Foundation, Inc.
 
    This file is part of the GNU C Library.
 
