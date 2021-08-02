5a5b9d071f9e51db04486c2355e4ee219d271ecc
Vineet Gupta <vgupta@synopsys.com>
2021-08-02 13:31:07 -0700

    ARC: cleanup jmp_buf-macros
    Non-functional change, helps reduce future updates for arc64.
    
    Signed-off-by: Vineet Gupta <vgupta@synopsys.com>

--- sysdeps/unix/sysv/linux/arc/jmp_buf-macros.h
+++ sysdeps/unix/sysv/linux/arc/jmp_buf-macros.h
@@ -1,6 +1,10 @@
-#define JMP_BUF_SIZE		(32 + 1 + 64 / (8 * sizeof (unsigned long int))) * sizeof (unsigned long int)
-#define SIGJMP_BUF_SIZE		(32 + 1 + 64 / (8 * sizeof (unsigned long int))) * sizeof (unsigned long int)
+#define ARC_NUMREG		32
+#define ARC_NUMSIGW		(64 / (8 * sizeof (unsigned long int)))
+
+/* see struct __jmp_buf_tag in generic code.  */
+#define JMP_BUF_SIZE		(ARC_NUMREG + 1 + ARC_NUMSIGW) * sizeof (unsigned long int)
+#define SIGJMP_BUF_SIZE		(ARC_NUMREG + 1 + ARC_NUMSIGW) * sizeof (unsigned long int)
 #define JMP_BUF_ALIGN		__alignof__ (unsigned long int)
 #define SIGJMP_BUF_ALIGN	__alignof__ (unsigned long int)
-#define MASK_WAS_SAVED_OFFSET	(32 * sizeof (unsigned long int))
-#define SAVED_MASK_OFFSET	(33 * sizeof (unsigned long int))
+#define MASK_WAS_SAVED_OFFSET	(ARC_NUMREG * sizeof (unsigned long int))
+#define SAVED_MASK_OFFSET	((ARC_NUMREG + 1) * sizeof (unsigned long int))
