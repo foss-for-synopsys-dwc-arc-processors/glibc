8254f4c3e610270108b46009c7bdae6ba1c12576
Pavel Kozlov <pavel.kozlov@synopsys.com>
2022-11-14 16:52:44 +0400

    ARCv3: update capability check of sqrt and fma builtins
    Add check for ARCv3. The compiler for ARCv3 always provides sqrt and
    fma hw fpu instructions for the hw fpu configuration.

--- sysdeps/arc/fpu/math-use-builtins-fma.h
+++ sysdeps/arc/fpu/math-use-builtins-fma.h
@@ -1,13 +1,17 @@
-#if defined __ARC_FPU_DP_FMA__
+#if defined __ARCV3__
 # define USE_FMA_BUILTIN 1
-#else
-# define USE_FMA_BUILTIN 0
-#endif
-
-#if defined __ARC_FPU_SP_FMA__
 # define USE_FMAF_BUILTIN 1
 #else
-# define USE_FMAF_BUILTIN 0
+# if defined __ARC_FPU_DP_FMA__
+#  define USE_FMA_BUILTIN 1
+# else
+#  define USE_FMA_BUILTIN 0
+# endif
+# if defined __ARC_FPU_SP_FMA__
+#  define USE_FMAF_BUILTIN 1
+# else
+#  define USE_FMAF_BUILTIN 0
+# endif
 #endif
 
 #define USE_FMAL_BUILTIN 0
--- sysdeps/arc/fpu/math-use-builtins-sqrt.h
+++ sysdeps/arc/fpu/math-use-builtins-sqrt.h
@@ -1,13 +1,17 @@
-#if defined __ARC_FPU_DP_DIV__
+#if defined __ARCV3__
 # define USE_SQRT_BUILTIN 1
-#else
-# define USE_SQRT_BUILTIN 0
-#endif
-
-#if defined __ARC_FPU_SP_DIV__
 # define USE_SQRTF_BUILTIN 1
 #else
-# define USE_SQRTF_BUILTIN 0
+# if defined __ARC_FPU_DP_DIV__
+#  define USE_SQRT_BUILTIN 1
+# else
+#  define USE_SQRT_BUILTIN 0
+# endif
+# if defined __ARC_FPU_SP_DIV__
+#  define USE_SQRTF_BUILTIN 1
+# else
+#  define USE_SQRTF_BUILTIN 0
+# endif
 #endif
 
 #define USE_SQRTL_BUILTIN 0
