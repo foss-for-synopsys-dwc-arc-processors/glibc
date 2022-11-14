c1fd20a5791337e824f9bf2ec8c4d9c8e0a160b6
Pavel Kozlov <pavel.kozlov@synopsys.com>
2022-11-14 16:52:44 +0400

    ARC:fpu: add extra capability check before use of sqrt and fma builtins
    For ARCHS add extra check for compiler definitions to ensure
    that compiler provides sqrt and fma hw fpu instructions else use
    software implementation

--- sysdeps/arc/fpu/math-use-builtins-sqrt.h
+++ sysdeps/arc/fpu/math-use-builtins-sqrt.h
@@ -1,4 +1,18 @@
-#define USE_SQRT_BUILTIN 1
-#define USE_SQRTF_BUILTIN 1
+#if defined __ARCV3__
+# define USE_SQRT_BUILTIN 1
+# define USE_SQRTF_BUILTIN 1
+#else
+# if defined __ARC_FPU_DP_DIV__
+#  define USE_SQRT_BUILTIN 1
+# else
+#  define USE_SQRT_BUILTIN 0
+# endif
+# if defined __ARC_FPU_SP_DIV__
+#  define USE_SQRTF_BUILTIN 1
+# else
+#  define USE_SQRTF_BUILTIN 0
+# endif
+#endif
+
 #define USE_SQRTL_BUILTIN 0
 #define USE_SQRTF128_BUILTIN 0
